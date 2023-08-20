4cc1c5abc83f1054b4d3f95b45e1fadfa3c209e5
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-08-19 22:02:43 -0700

    topAppBar and bottomAppbar done

--- app/src/main/java/com/example/instagramprofileclone/classes/SettingsScreen.kt
+++ app/src/main/java/com/example/instagramprofileclone/classes/SettingsScreen.kt
@@ -5,5 +5,6 @@ import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class SettingsScreen(val route: String, val title: String, image: ImageVector) {
+
     object Settings: SettingsScreen("setting", "Setting", Icons.Outlined.Settings)
 }
